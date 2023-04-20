330a51cf3c0af50659e10c1eb90cf1cf23e444f9
Luis Carvalho <lapc19@hotmail.com>
2023-04-20 09:36:00 +0100

    update

--- csvRepeater.js
+++ csvRepeater.js
@@ -7,7 +7,7 @@ import { run as runCSVMode } from './modes/csv.js';
 import { analyseECommerceDomain } from './contexts/ecommerce.js';
 import { zipDomainFolder,  zipDataAndErrorFolder, removeFolders} from './utils.js';
 
-const path = '/Users/luiscarvalho/accessibility-crawler/ecommercecsvs/';
+const path = './ecommercecsvs/';
 
 let files = fs.readdirSync(path);
 for(let i = 0; i < files.length; i++) {
