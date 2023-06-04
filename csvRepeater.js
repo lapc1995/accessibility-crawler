0be4c18bbea023e155dcb9bac1b8462bcec4347f
Luis Carvalho <lapc19@hotmail.com>
2023-06-04 23:19:19 +0100

    update

--- csvRepeater.js
+++ csvRepeater.js
@@ -4,7 +4,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 import { run as runCSVMode } from './modes/csv.js';
-import { analyseECommerceDomain } from './contexts/ecommerce.js';
+import { analyseLargeScaleDomain } from './contexts/largeScale.js';
 import { zipDomainFolder,  zipDataAndErrorFolder, removeFolders} from './utils.js';
 
 const path = './ecommercecsvs/';
@@ -12,7 +12,7 @@ const path = './ecommercecsvs/';
 let files = fs.readdirSync(path);
 for(let i = 0; i < files.length; i++) {
     process.env.CSVFILEPATH = `${path}${files[i]}`;
-    await runCSVMode(analyseECommerceDomain);
+    await runCSVMode(analyseLargeScaleDomain);
     await zipDataAndErrorFolder(files[i]);
     removeFolders('./data', './error');
 }
