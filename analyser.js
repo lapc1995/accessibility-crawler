442f4ee0b2769095cf45d4106e0a154ffe78ec0a
Luis Carvalho <lapc19@hotmail.com>
2023-08-07 17:39:35 +0100

    wip

--- analyser.js
+++ analyser.js
@@ -26,6 +26,7 @@ export const getReportForURLParallel = async(url, browser, options = {}) => {
         await waitForBrowser();
 
         await delay(1000);
+        console.log(browser);
         let userAgent = await browser.userAgent();
         userAgent += " crawled for https://luiscarvalho.dev/accessibilityStudy/";
 
