e126e612db1d09d6215887ee37ed895b6a3c94ac
Luis Carvalho <lapc19@hotmail.com>
2023-08-15 22:46:14 +0100

    update

--- keepAliveWatcher.js
+++ keepAliveWatcher.js
@@ -1,15 +1,59 @@
-//const { exec } = require();
+import util from 'util';
+import { exec as execCallback } from 'child_process';
+const exec = util.promisify(execCallback);
 
-import { exec } from "child_process";
+//import { exec } from "child_process";
 
-exec("ls -la", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
+import * as chokidar from 'chokidar';
+
+let timeoutId;
+
+async function restartCrawler() {
+
+    const command = 'pm2 stop ./csvRepeater.js'; 
+    let { stdout, stderr } = await exec(command);
+    if(stderr) {
+        console.log(stderr);
         return;
     }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
+    console.log(stdout);
+
+    const command2 = 'pm2 start ./csvRepeater.js';
+    let{ stdout: stdout2, stderr: stderr2 } = await exec(command2);
+    if(stderr2) {
+        console.log(stderr2);
         return;
     }
-    console.log(`stdout: ${stdout}`);
-});
+    console.log(stdout2);
+
+    timeoutId = setTimeout(restartCrawler, timeout);
+}
+
+
+const timeout = 1000 * 60 * 30;
+
+
+const watcher = chokidar.watch('./largeScaleDB.json', {
+    persistent: true
+  });
+
+const log = console.log.bind(console);
+
+const OnFileChange = () => {
+    console.log("File changed");
+    resetTimer();
+}
+
+watcher
+.on('add', path => log(`File ${path} has been added`))
+.on('change', path => log(`File ${path} has been changed`))
+
+
+timeoutId = setTimeout(restartCrawler, timeout);
+
+function resetTimer() {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(restartCrawler, timeout);
+    console.log("Timer reset");
+}
+
