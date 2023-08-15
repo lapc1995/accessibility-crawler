01fa9f431fceaae2f9604a6234540397652368c5
Luis Carvalho <lapc19@hotmail.com>
2023-08-15 17:56:00 +0100

    keep alive starter

---
+++ keepAliveWatcher.js
@@ -0,0 +1,15 @@
+//const { exec } = require();
+
+import { exec } from "child_process";
+
+exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
